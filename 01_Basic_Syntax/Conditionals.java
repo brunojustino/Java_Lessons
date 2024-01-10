b39053536fa1d67e63c834e82c677ff71cafa224
brunojustino <8812217+brunojustino@users.noreply.github.com>
2024-01-10 13:05:51 -0300

    Added Conditionals code

---
+++ 01_Basic_Syntax/Conditionals.java
@@ -0,0 +1,24 @@
+class Conditionals {
+
+    public static void main(String[] args) {
+
+        if (true) {
+            System.out.println("The main and most used if statement");
+        } else {
+            System.out.println("False");
+        }
+
+        if (true) System.out.println("No curly braces needed if has only 1 line");
+
+        if (false)
+            System.out.println("No curly braces needed if only 1 line");
+        else
+            System.out.println("You can have a else without curly braces too");
+
+        //With tenary operator
+        // Good for short expressions and to use in the return of a method. Can make your code harder to read.
+        String foundUser = (true) ? "User found" : "User not found";
+        System.out.println(foundUser);
+
+    }
+}
